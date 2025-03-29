029a2ee4fc49017caffd802444d5edfdfc823983
Lokesh Reddy <bommireddylokesh2000@gmail.com>
2025-03-29 20:06:31 +0530

    pest

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 ALLOWED_HOSTS = ['https://pest-9e7o.onrender.com','localhost' ]
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
