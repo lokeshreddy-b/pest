0e4dc6d7cfa45d48871be8d27ea5d4d8b406cc9e
Lokesh Reddy <bommireddylokesh2000@gmail.com>
2025-03-29 20:05:07 +0530

    pest

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-ALLOWED_HOSTS = ['your-render-app.onrender.com','localhost' ]
+ALLOWED_HOSTS = ['https://pest-9e7o.onrender.com','localhost' ]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
