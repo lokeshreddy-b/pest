4198c9fd237eab5ea867fbbb63f6c8f9af7672d5
lokeshreddy-b <bommireddylokesh2000@gmail.com>
2025-03-29 20:41:06 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-ALLOWED_HOSTS = ['https://pest-9e7o.onrender.com', '127.0.0.1','localhost' ]
+ALLOWED_HOSTS = ['pest-9e7o.onrender.com', '127.0.0.1','localhost' ]
 
 # 
 BASE_DIR = Path(__file__).resolve().parent.parent
